e216bde368365d4138733dfa54983e2bd2d1e9ab
riagupta3 <110932784+riagupta3@users.noreply.github.com>
2022-08-21 13:09:42 -0700

    Update merge.py

--- merge.py
+++ merge.py
@@ -4,3 +4,20 @@ import os
 import subprocess
 import sys
 from Bio import SeqIO
+
+# Need Auxillary functions section?
+
+# Parse input json
+with open("/batchx/input/input.json", "r") as inputFile:
+    inputJson = inputFile.read()
+parsedJson = json.loads(inputJson)
+
+# BX_MEMORY & BX_VCPUS
+bxMemory = os.environ['BX_MEMORY']
+bxVcpus = os.environ['BX_VCPUS']
+
+# intervalFile
+intervalFile = parsedJson["intervalFile"]
+
+# need section that follows in getfasta file?
+
